a4c794c8d61d30a65f815524416da6c839d2dd1c
EduBN14 <psoeduardo1407@gmail.com>
2025-07-26 16:01:32 -0500

    agrego forEach para el catalogo

---
+++ src/db/catalgoDB.ts
@@ -0,0 +1,28 @@
+import type { GalletasInter } from "../models/galletas_inter";
+
+export const galletasDB: GalletasInter[] = [
+    {
+        nombre: "Galleta chocochips",
+        descripcion: "es una galleta clásica de vainilla con trozos de chocolate.",
+    },
+    {
+        nombre: "Galleta redvelvet",
+        descripcion: "Una galleta aterciopelada color carmesí, con suaves matices de cacao y con chips de chocolate blanco.",
+    },
+    {
+        nombre: "Galleta full chocolate",
+        descripcion: "Una galleta intensa de cacao, con trozos fundentes y una textura húmeda que se deshace en un festín de chocolate en cada mordisco.",
+    },
+    {
+        nombre: "Brownie",
+        descripcion: "Un brownie denso y húmedo, con bordes crujientes y una delicada capa de azúcar impalpable que realza su intenso sabor a chocolate.",
+    },
+    {
+        nombre: "Pye de manzana",
+        descripcion: "Un pie de manzana de corteza dorada y crujiente, relleno de manzanas jugosas con un suave toque de canela.",
+    },
+    {
+        nombre: "Rollo de canela",
+        descripcion: "Un rollo de canela tierno y esponjoso, con espirales perfumadas de canela y un glaseado dulce que se funde en cada bocado.",
+    },
+    ];
---
+++ src/models/galletas_inter.ts
@@ -0,0 +1,4 @@
+export interface GalletasInter {
+  nombre: string; // Nombre de la galleta
+    descripcion: string; // Descripción de la galleta
+    };
